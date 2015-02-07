a357c75d3f25e432b9bb9eea5be5bb261b484b7e
Nicholas Bannister-Andrews <nbannist@gmail.com>
2015-02-06 19:13:02 -0500

    Trying to split app so it can be used on App Engine or Heroku. "Hold on to your butts!"

--- monkietoes.go
+++ monkietoes.go
@@ -1,29 +1,37 @@
 package monkietoes
 
 import (
-    //"fmt"
-    "html/template"
-    "net/http"
-   	"github.com/gorilla/mux"
+	//"fmt"
+	"github.com/gorilla/mux"
+	"html/template"
+	"net/http"
 )
 
+// For Non-AppEngine Environments
+func main() {
+	http.HandleFunc("/", hello)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
+
 func init() {
-	// 
+	//
 	appRouter := mux.NewRouter()
 	appRouter.HandleFunc("/", simpleHello)
 
-	// 
+	//
 	http.Handle("/", appRouter)
 }
 
 var welcome = template.Must(template.ParseFiles(
-  "templates/_app-base.template.html",
-  "templates/welcome.template.html",
+	"templates/_app-base.template.html",
+	"templates/welcome.template.html",
 ))
 
 func simpleHello(w http.ResponseWriter, req *http.Request) {
-    if err := welcome.Execute(w, nil); err != nil {
-      http.Error(w, err.Error(), http.StatusInternalServerError)
-  }
+	if err := welcome.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
-
---
+++ tests/README.md
@@ -0,0 +1,6 @@
+MonkieToes Testing
+------------------
+
+All tests in /tests/
+
+
